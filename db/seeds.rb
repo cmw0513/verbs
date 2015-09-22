b8936f75e428600627e018cfe322c0532909eb19
Christina Wolfe <cmw0513@gmail.com>
2015-09-22 16:15:35 +0200

    added 3 verbs as seed objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+PrepositionVerb.create(verb: 'kiezen', translation: 'choose', preposition: 'voor', sentence_with_blank: 'Ik heb daar niet _____ gekozen', test_date: Date.new(2015,9,23))
+PrepositionVerb.create(verb: 'klagen', translation: 'complain', preposition: 'over', sentence_with_blank: 'Zij klaagde steeds _____ de buren', test_date: Date.new(2015,9,23))
+PrepositionVerb.create(verb: 'leiden', translation: 'lead', preposition: 'tot', sentence_with_blank: 'Deze maatregel leidt _____ niets!', test_date: Date.new(2015,9,23))
+
